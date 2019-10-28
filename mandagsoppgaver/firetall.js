7e2d03fbe7c7d071374e6d53fcc9921ff91e71bd
Kim Roar Foldøy Hauge <zokum@oppetid.no>
2019-10-28 10:08:30 +0000

    Fiks: Sorter etter verdi, ikke alfa

--- mandagsoppgaver/firetall.js
+++ mandagsoppgaver/firetall.js
@@ -4,9 +4,9 @@ function firetall(a, b, c, d) {
 	tall[1] = b;
 	tall[2] = c;
 	tall[3] = d;
-	tall.sort();
+	tall.sort( function(a, b){return a-b} );
 	console.log("Minst: " + tall[0] + ", størst: " + tall[3] + ", gjennomsnitt: " + ( (a+b+c+d) / 4.0));
 }
-firetall(1,2,3,4);
+firetall(4,3,2,1);
 firetall(11,11,80,90);
 firetall(2,4,9,12);
